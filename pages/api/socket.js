164cd0027a1c6123bca8eb3c383f495776401f54
GabSnow24 <biel_surf159@hotmail.com>
2021-10-19 20:18:15 -0300

    Socket Finished

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,22 +1,31 @@
-import cors from 'cors'
-
+// import cors from 'cors'
+const cors = require('cors')
 const api = async () =>{
 const app = require('express')()
+app.use(cors({origin:"*"}))
 const http = require('http').Server(app)
 const io = require('socket.io')(http, {
     cors: {
-        origin: "https://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
+        // allowedHeaders: ["my-custom-header"],
+        // credentials: true
       }
     
 })
 
-app.use(cors)
 io.on('connection', (socket) =>{
-    console.log('[IO] - rodando 💓')
+    console.log(`[IO] - rodando 💓 ${socket.id}`)
+    
+    socket.on('teste', (data)=>{
+
+        console.log(data)
+
+        console.log('teste funcionou')
+    })
 })
+
+
 
 http.listen(3003, ()=>{
     console.log('[HTTP] - rodando na 3003 bro 💟')
